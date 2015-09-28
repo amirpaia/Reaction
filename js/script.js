8bf8c77d09f888c11fabe3eec6531d4718cd03bb
amirpaia <amirpaia@gmail.com>
2015-09-29 00:30:38 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 	var keys = ['v','g','h','m'];
 	var interval, cursor=-1, n=0, blockIndex=0;
 	var seq=[];//= getRandomSequence(12);
+	var constantSeq = getRandomSequence(12);
+	var isStop = false;
 	for (i=1; i<=8; i++){
 		for (j=1; j<=10; j++){
 			var tmpSeq;
@@ -11,7 +13,8 @@ $(document).ready(function(){
 				tmpSeq = getRandomSequence(12);
 			}
 			else if (j==1){
-				tmpSeq = getRandomSequence(12);
+				//tmpSeq = getRandomSequence(12); 
+				tmpSeq = constantSeq; // تمام 10 سري 12 تايي در همه بلوک ها يکسان باشد
 			}
 			seq = seq.concat(tmpSeq);
 		}
@@ -20,38 +23,51 @@ $(document).ready(function(){
 	var delay = 1000;
 	var clearDelay = 500;
 	
-	clearStars();
-	start();
 	
 	$(document).keypress(function(event){
-		var selectedKey= String.fromCharCode(event.which).toLowerCase();
-		//console.log(selectedKey);
-		var index = keys.indexOf(selectedKey);
-		if (index >= 0){
-			//console.log(index);
-			if ($('.stars span').eq(index).text()== '*'){
-				//$('.timesheet').append(); //console.log("True!!!");
-				
-				clearStars();
-				clearInterval(interval);
-				console.log('FUUU!');
-				n=0;
-				start();
+		if (event.which == 32){
+			clearStars();
+			start();
+		}
+		else{
+			var selectedKey= String.fromCharCode(event.which).toLowerCase();
+			//console.log(selectedKey);
+			var index = keys.indexOf(selectedKey);
+			if (index >= 0){
+				//console.log(index);
+				if ($('.stars span').eq(index).text()== '*'){
+					//$('.timesheet').append(); //console.log("True!!!");
+					
+					clearStars();
+					clearInterval(interval);
+					console.log('FOOO!');
+					n=0;
+					start();
+				}
+				else
+					;
 			}
-			else
-				;
 		}
 	});
 	function start(){
 		interval = setInterval(function() {
 			if (cursor == seq.length-1){
 				clearInterval(interval);
+				$("#show").show();
 				return;
 			}
 			
 			n++;
 			if (n % 3 == 1){ // run after 500 ms
 				cursor++;
+				if (isStop && cursor % 120 == 0){
+					isStop = false;
+					cursor--;
+					//alert("Finish!");
+					clearInterval(interval);
+					return;
+				}
+				isStop = true;
 				showStar(seq[cursor]);
 			}
 			else if (n % 3 == 0){ // run after 1500 ms
@@ -85,6 +101,8 @@ $(document).ready(function(){
 			$('.timesheet').append('Start Block ' + blockIndex + " : " + timestamp +"<br/>");
 		}
 		console.log(cursor + " - " + keys[value-1]+ " : " + timestamp);
+		$('.timesheet').append(cursor + " - " + keys[value-1]+ " : " + timestamp + "<br/>");
+		
 		$('.stars span').eq(value-1).text('*');
 		
 		if (cursor % 120 == 119){
@@ -96,4 +114,8 @@ $(document).ready(function(){
 	function clearStars(){
 		$('.stars span').text('');
 	}
+	
+	$("#show").click(function(){
+		$(".timesheet").show();
+	});
 });
